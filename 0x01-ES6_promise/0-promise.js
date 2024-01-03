fdaa51c4f9ebd0f6f73720f4256b3f167757c030
Adam <adam@gmail.com>
2024-01-03 22:04:34 +0000

    0.task

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,11 @@
 export default function getResponseFromAPI(){
   const p = new Promise((resolve, reject) => {
-    resolve('This is a promise');
+    let a = 0;
+    if (a == 0){
+      resolve('This is a promise');
+    } else{
+      reject('This is rejected promise');
+    }
   });
 
   return p;
