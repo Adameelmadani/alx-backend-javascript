6b224862e5239cf4317b22e8d30b3a93baadcaee
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2024-01-03 23:13:15 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,3 @@
 export default function getResponseFromAPI(){
-  const p = new Promise((resolve, reject) => {
-    let a = 0;
-    if (a == 0) {
-      resolve('This is a promise');
-    } else {
-      reject('This is rejected promise');
-    }
-  });
-          return p;
+  return new Promise(() => {});
 }
