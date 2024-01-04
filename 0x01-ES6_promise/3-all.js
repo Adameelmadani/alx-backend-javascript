082406b835d4e5c5bfa3021a091ba422d04244b5
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2024-01-04 10:43:35 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,6 +4,6 @@ export default function handleProfileSignup() {
   const p1 = uploadPhoto();
   const p2 = createUser();
   Promise.all([p1, p2])
-  .then((messages) => (console.log(messages)))
+  .then((messages) => (console.log(`${messages[0].body} ${messages[1].firstName} ${messages[1].lastName}`)))
   .catch(() => (console.log('Signup system offline')));
 }
