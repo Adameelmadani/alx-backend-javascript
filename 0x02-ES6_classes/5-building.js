2b4140f03abece56b9d6a8ae794111a448aa7ddc
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2024-01-04 23:05:24 +0100

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,12 @@
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building && evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(sqft) {
-    this._sqft = sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
