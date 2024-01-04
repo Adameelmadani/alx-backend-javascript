b84cb9a3808baea47a6c1ef780e34c202f61ce15
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2024-01-04 23:23:20 +0100

    Update 7-airport.js

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -3,4 +3,8 @@ export default class Airport {
     this._name = name;
     this._code = code;
   }
+
+  toString() {
+    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}'`;
+  }
 }
