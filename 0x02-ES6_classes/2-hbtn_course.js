905659d33081648c22ad12c1fcfda8a67bcde90a
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2024-01-04 22:08:56 +0100

    Update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -23,29 +23,29 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
-  
+
   set name(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
   }
-  
+
   get length() {
     return this._length;
   }
-  
+
   set length(length) {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
   }
-  
+
   get students() {
     return this._students;
   }
-  
+
   set students(students) {
     if (!Array.isArray(students)) {
       throw new TypeError('Students must be an array of strings');
