89a246d8c78684194a4fda596e39753e2c5a1cbe
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2024-01-05 00:35:54 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,11 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const array = [];
-  Promise.all([signUpUser(), uploadPhoto()])
-  .then((messages) => {array.push(messages)
-    return;})
-  .catch();
-
-  return array;
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => {
+      const array = [];
+      for (const result of results) {
+        if (result.status === 'fulfilled') {
+          array.push({ status: result.status, value: result.value });
+        }
+        else {
+          array.push({ status: result.status, value: `${result.reason}` });
+        }
+      }
+      return array;
+    });
 }
