c463da54396af9cdd22c0fc18415e5e33ef4c792
Adam Elmadani <adamelmadani0129@gmail.com>
2024-01-10 15:47:54 +0000

    8.task

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,4 @@
 export default function cleanSet(set, sSt) {
-  return set.filter((elem) => elem.startsWith(sSt)).join('-');
+  return [...set].filter((elem) => elem.startsWith(sSt) && sSt !== '')
+    .map((elem) => elem.replace(sSt, '')).join('-');
 }
