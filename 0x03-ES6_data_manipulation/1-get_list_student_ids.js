b0e8447a5d82aa3eb99c5ef10fc038f49d374408
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2024-01-10 02:10:33 +0100

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
 export default function getListStudentIds(student_list) {
-  if(Array.isArray(student_list)) {
+  if (Array.isArray(student_list)) {
     return student_list.map((student) => student.id);
   }
   return [];
