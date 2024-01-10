eb7484d6eae5a836454502c2ee974559ae52201e
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2024-01-10 01:42:49 +0100

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 export default function getListStudents() {
   const list_obj = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 1, firstName: "Guillaume", location: "San Francisco" },
+    { id: 2, firstName: "James", location: "Columbia" },
+    { id: 5, firstName: "Serena", location: "San Francisco" }
   ];
   return list_obj;
 }
