b9f7628d041668fd7de6e82fadb1eda6060eb793
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2024-01-10 16:55:15 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
-export default function cleanSet(set, sSt) {
+export default function cleanSet(set, sSt='') {
+  if (typeof sSt !== 'string') {
+    return '';
+  }
   return [...set].filter((elem) => elem.startsWith(sSt) && sSt !== '')
     .map((elem) => elem.replace(sSt, '')).join('-');
 }
