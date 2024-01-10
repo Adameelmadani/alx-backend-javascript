078ddf6477ce08df730d873170a3167ddb6f9589
Adam Elmadani <adamelmadani0129@gmail.com>
2024-01-10 15:27:51 +0000

    7.task

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,9 @@
 export default function hasValuesFromArray(set, array) {
-  return set.isSupersetOf(new Set(array));
+  const set1 = new Set(array);
+  for (const elem of set1) {
+    if (!set.has(elem)) {
+      return false;
+    }
+  }
+  return true;
 }
