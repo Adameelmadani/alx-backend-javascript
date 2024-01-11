6a626634e331d5d57c309d00d8ee8cbfe3dcf856
Adameelmadani <125741857+Adameelmadani@users.noreply.github.com>
2024-01-11 14:55:38 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
-export default function cleanSet(set, sSt='') {
-  if (typeof sSt !== 'string') {
-    return '';
-  }
-  return [...set].filter((elem) => elem.startsWith(sSt) && sSt !== '')
-    .map((elem) => elem.replace(sSt, '')).join('-');
+export default function cleanSet(set, startString) {
+  if (!startString || !set || !(set instanceof Set) || typeof startString !== 'string') return '';
+  return Array.from(set)
+    .filter((val) => val && val.startsWith(startString))
+    .map((val) => val.replace(startString, ''))
+    .join('-');
 }
