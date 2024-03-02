d3118639a0c2d3530d48dcf341c4556bcf4bc1ee
Adam Elmadani <adamelmadani0129@gmail.com>
2024-03-02 11:31:51 +0000

    1.task fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,10 @@
-process.stdin.setEncoding('utf-8');
-console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('readable', function() {
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
   const input = process.stdin.read();
-  console.log('Your name is: ${input}');
-  console.log('This important software is now closing');
+  if (input) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
+});
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
